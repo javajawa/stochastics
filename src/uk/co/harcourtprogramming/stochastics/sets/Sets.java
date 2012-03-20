8abb645606477ddb92866ec55779d56216d7202e
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-03-20 12:51:42 +0000

    Add Integers and Naturals sets

--- src/uk/co/harcourtprogramming/stochastics/sets/Sets.java
+++ src/uk/co/harcourtprogramming/stochastics/sets/Sets.java
@@ -8,6 +8,11 @@ public final class Sets
 	public final static Range<Double> REALS =
 		new ContinuousDoubleRange(Double.MIN_VALUE, Double.MAX_VALUE);
 
+	public final static Range<Integer> INTEGERS =
+		new IntRange(Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+	public final static Range<Integer> NATURALS = INTEGERS.tailSet(1);
+
 	private Sets()
 	{
 		// Nothing to see here. Move along, citizen!
