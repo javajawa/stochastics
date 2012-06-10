06dbf3f1564ac109aedcfbd86df13d042da4ebfa
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-06-10 21:15:21 +0100

    Add Maths.Trigger functions

--- src/uk/co/harcourtprogramming/stochastics/helpers/Maths.java
+++ src/uk/co/harcourtprogramming/stochastics/helpers/Maths.java
@@ -24,6 +24,81 @@ public final class Maths
 		return Math.max(min, Math.min(value, max));
 	}
 
+	public static boolean trigger(boolean curr, double value, double on, double off)
+	{
+		if (on > off)
+		{
+			if (value > on) return true;
+			if (value < off) return false;
+		}
+		else
+		{
+			if (value < on) return true;
+			if (value > off) return false;
+		}
+		return curr;
+	}
+
+	public static boolean trigger(boolean curr, int value, int on, int off)
+	{
+		if (on > off)
+		{
+			if (value > on) return true;
+			if (value < off) return false;
+		}
+		else
+		{
+			if (value < on) return true;
+			if (value > off) return false;
+		}
+		return curr;
+	}
+
+	public static boolean trigger(boolean curr, long value, long on, long off)
+	{
+		if (on > off)
+		{
+			if (value > on) return true;
+			if (value < off) return false;
+		}
+		else
+		{
+			if (value < on) return true;
+			if (value > off) return false;
+		}
+		return curr;
+	}
+
+	public static boolean trigger(boolean curr, float value, float on, float off)
+	{
+		if (on > off)
+		{
+			if (value > on) return true;
+			if (value < off) return false;
+		}
+		else
+		{
+			if (value < on) return true;
+			if (value > off) return false;
+		}
+		return curr;
+	}
+
+	public static <T extends Comparable<T>> boolean trigger(boolean curr, T value, T on, T off)
+	{
+		if (on.compareTo(off) == 1)
+		{
+			if (value.compareTo(on) == 1) return true;
+			if (value.compareTo(off) == -1) return false;
+		}
+		else
+		{
+			if (value.compareTo(on) == -1) return true;
+			if (value.compareTo(off) == 1) return false;
+		}
+		return curr;
+	}
+
 	private Maths()
 	{
 	}
